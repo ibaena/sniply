4cd0ee62af25c67e247a46990ff554f6aced5322
ivanockupy <ivan@ockupy.com>
2017-05-07 12:56:46 -0400

    click counter added

--- server/main.js
+++ server/main.js
@@ -18,7 +18,7 @@ function onRoute(req, res, next) {
   const link = Links.findOne({token:req.params.token});
   // if link obj found redirect user
   if (link) {
-    console.log(link);
+    Links.update(link,{ $inc: {clicks:1} });
 
     res.writeHead(307,{'Location': link.url});
     res.end();
